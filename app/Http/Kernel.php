5e7e391cbde6c26682f567e9bee8bae3439050bd
Jaisy <abdurrahmanjaisy@mail.ugm.ac.id>
2020-05-02 17:55:54 +0700

    publish cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,6 +19,8 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
+        \Barryvdh\Cors\HandleCors::class,
+
         \App\Http\Middleware\Cors::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
         /*
          * Application Service Providers...
          */
+        Barryvdh\Cors\ServiceProvider::class,
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
