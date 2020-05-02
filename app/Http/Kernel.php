036bf95366b0183d843b9f7b6e8f91328b15f093
Jaisy <abdurrahmanjaisy@mail.ugm.ac.id>
2020-05-02 16:17:46 +0700

    publish cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,11 +15,10 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \App\Http\Middleware\TrustProxies::class,
-        // \Fruitcake\Cors\HandleCors::class,
+        \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
-        \App\Http\Middleware\Cors::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
 
